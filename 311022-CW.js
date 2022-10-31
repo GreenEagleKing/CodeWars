c6b394c419ba0f88d4a423031dc73fd28ac23ef4
Ben King <benking1293@gmail.com>
2022-10-31 10:58:53 +0000

    "Redo 2/5 array challenge"

---
+++ 311022-CW.js
@@ -0,0 +1,15 @@
+// 2/5 Array REDO
+
+// 7 kyu Sum of odd numbers
+
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) 
+
+// integers, positive, 
+// sum of numbers on row, n * n * n
+
+function rowSumOddNumbers(n) {
+	return n * n * n
+}
+
+console.log(rowSumOddNumbers(3)) // 27 - 3 * 3 = 9 * 3 = 27
+console.log(rowSumOddNumbers(2)) // 8 - 2*2 = 4 * 2 = 8
